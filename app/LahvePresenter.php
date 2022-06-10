b2dbe334ee1627cd8b544b0cb7fd36ccaf5c2a61
Filip Oščádal <f@mxd.cz>
2022-06-10 06:45:34 +0200

    minor update

---
+++ app/LahvePresenter.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * GSC Tesseract
+ *
+ * @author   Fred Brooker <oscadal@gscloud.cz>
+ * @license  MIT https://gscloud.cz/LICENSE
+ */
+
+namespace GSC;
+
+use Cake\Cache\Cache;
+use Michelf\MarkdownExtra;
+
+/**
+ * Mini Presenter
+ */
+class LahvePresenter extends APresenter
+{
+    /**
+     * Main controller
+     *
+     * @return self
+     */
+    public function process()
+    {
+        // basic setup
+        $data = $this->getData();
+        $presenter = $this->getPresenter();
+        $view = $this->getView();
+        $this->checkRateLimit()->setHeaderHtml()->dataExpander($data); // data = Model
+
+        // process advanced caching
+        $use_cache = (bool) (DEBUG ? false : $data["use_cache"] ?? false);
+        $cache_key = hash("sha256", join("_", [$data["host"], $data["request_path"], "htmlpage"]));
+        if ($use_cache && $output = Cache::read($cache_key, "page")) {
+            header("X-Cached: true");
+            return $this->setData("output", $output .= "\n<script>console.log('*** page content cached');</script>");
+        }
+
+        // output
+        $output = $this->setData($data)->renderHTML($presenter[$view]["template"]); // render
+        StringFilters::trim_html_comment($output); // fix content
+        Cache::write($cache_key, $output, "page"); // save cache
+        header("X-Cached: false");
+        return $this->setData("output", $output); // save model
+    }
+}
