55d51ee197a72cacfbc2cbc5075376a76e800248
Filip Oščádal <f@mxd.cz>
2022-09-23 08:22:22 +0200

    minor update

--- app/ApiPresenter.php
+++ app/ApiPresenter.php
@@ -430,7 +430,7 @@ class ApiPresenter extends APresenter
             }
         }
         foreach ($groups as $k => $v) {
-            if (\count($v) < 3) {
+            if (\count($v) < 2) {
                 unset($groups[$k]);
             }
         }
