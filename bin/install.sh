1a36cee9ef8aef50f2e8a9c5cd797adf04f3aa33
Filip Oščádal <f@mxd.cz>
2023-01-06 08:28:48 +0100

    automatic update

--- bin/install.sh
+++ bin/install.sh
@@ -13,9 +13,9 @@ echo "We need a root permission to run some tasks 😎"
 sudo chmod 0777 ci data logs temp www/download www/upload
 sudo chown -R www-data:www-data data
 sudo chgrp -R www-data ci data www www/cdn-assets www/download www/upload
-sudo apt-get install -yq libapache2-mod-php8.0 openssl php-imagick php-redis \
-  php8.0 php8.0-cli php8.0-curl php8.0-gd php8.0-intl php8.0-mbstring php8.0-readline php8.0-xml php8.0-zip
-sudo apt-get install php8.0-redis php8.0-imagick
+sudo apt-get install -yq libapache2-mod-php8.2 openssl php-imagick php-redis \
+  php8.2 php8.2-cli php8.2-curl php8.2-gd php8.2-intl php8.2-mbstring php8.2-readline php8.2-xml php8.2-zip
+sudo apt-get install php8.2-redis php8.2-imagick
 sudo a2enmod php8.0 expires headers rewrite
 
 command -v composer >/dev/null 2>&1 || fail PHP composer is not installed!
