67182b9f3436efa2319d4f9adc63818c3500f946
Filip Oščádal <f@mxd.cz>
2023-01-11 17:01:05 +0100

    automatic update

--- app/LahvePresenter.php
+++ app/LahvePresenter.php
@@ -41,23 +41,9 @@ class LahvePresenter extends APresenter
         $view = $this->getView();
         $this->checkRateLimit()->setHeaderHtml()->dataExpander($data);
 
-        // process advanced caching
-        $use_cache = (bool) (DEBUG ? false : $data["use_cache"] ?? false);
-        $cache_key = hash(
-            "sha256", join(
-                "_",
-                [$data["host"], $data["request_path"], "htmlpage"]
-            )
-        );
-        if ($use_cache && $output = Cache::read($cache_key, "page")) {
-            header("X-Cached: true");
-            return $this->setData("output", $output);
-        }
-
         // process output
         $output = $this->setData($data)->renderHTML($presenter[$view]["template"]);
         StringFilters::trim_html_comment($output);
-        Cache::write($cache_key, $output, "page");
         header("X-Cached: false");
         return $this->setData("output", $output);
     }
